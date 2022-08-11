f52f62f6ef2b1b12bd33a8882a88cb8293dfa34b
Eldor Azimov <eldorazimov37@gmail.com>
2022-08-11 05:34:04 -0700

    Adding a Modal via a React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = () => {
   const cartItems = (
@@ -10,17 +11,17 @@ const Cart = () => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.action}>
-        <button className={classes["button--alt"]}>close</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 

903fd6d4b203f10678197fd4ae368880cae0002d
Eldor Azimov <eldorazimov37@gmail.com>
2022-10-06 13:30:14 +0900

    Adding a Chekout Form

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,34 @@
+import classes from "./Checkout.module.css";
+
+const Chekout = (props) => {
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal</label>
+        <input type="text" id="postal" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" />
+      </div>
+      <button type="button" onClick={props.onCansel}>
+        Cansel
+      </button>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Chekout;
